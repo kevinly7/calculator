fe6ec55fd3cde099ef5247dfbd2721e3e176d096
Kevin Ly <kevinly7@uw.edu>
2015-10-06 22:13:55 -0700

    initial setup

---
+++ SimpleCalc/SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Kevin Ly on 10/6/15.
+//  Copyright © 2015 Kevin Ly. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

1c2a92a05684a78ff9eabdb883d33c527b287936
Kevin Ly <kevinly7@uw.edu>
2015-10-08 20:17:41 -0700

    tested and completed project

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -8,7 +8,12 @@
 
 import Foundation
 
-print("Hello, World!")
+print("--SimpleCalc--")
+
+print("Basic Operations (+,-,*,/): enter a number, hit (enter), enter an operation, hit (enter), enter the second number, hit (enter).")
+print("")
+print("Advanced Opeations: enter a number, or string of numbers, followed by an operations (count, avg, fact) and seperating each value with spaces")
+print("")
 
 func input() -> String {
     let keyboard = NSFileHandle.fileHandleWithStandardInput()
@@ -21,21 +26,66 @@ func convert(incoming:String) -> Int {
     return NSNumberFormatter().numberFromString(incoming)!.integerValue
 }
 
-var firstNum : Int = convert(input())
-var operand : String = input()
-var secondNum : Int = convert(input())
+var test = "no"
 
-switch operand {
-    case "+", "add":
-        print(firstNum + secondNum)
-    case "-", "sub":
-        print(firstNum - secondNum)
-    case "*", "mul":
-        print(firstNum * secondNum)
-    case "/", "div":
-        print(firstNum / secondNum)
-    case "%", "mod":
-        print(firstNum % secondNum)
-    default:
-        print("You did not provide a valid operand.")
-}
+repeat {
+    let firstInput = input().characters.split{$0 == " "}.map(String.init)
+
+    if (firstInput.count == 1) {
+        let firstNum = Double(convert(firstInput[0]))
+        let operand = input()
+        let secondNum = Double(convert(input()))
+        switch operand {
+            case "+", "add":
+                print("Answer: \(firstNum + secondNum)")
+                print("")
+            case "-", "sub":
+                print("Answer: \(firstNum - secondNum)")
+                print("")
+            case "*", "mul":
+                print("Answer: \(firstNum * secondNum)")
+                print("")
+            case "/", "div":
+                print("Answer: \(firstNum / secondNum)")
+                print("")
+            case "%", "mod":
+                print("Answer: \(firstNum % secondNum)")
+                print("")
+            default:
+                print("You did not provide a valid operand.")
+                print("")
+        }
+    } else {
+        var arrayOfNumbers = [Int]()
+        for var index = 0; index < firstInput.count - 1; index++ {
+            arrayOfNumbers.append(convert(firstInput[index]))
+        }
+        let operand = firstInput[firstInput.count-1]
+        switch operand {
+            case "count":
+                print("You entered \(arrayOfNumbers.count) numbers.")
+                print("")
+            case "avg":
+                var avg : Double = 0;
+                for var index = 0; index < arrayOfNumbers.count; index++ {
+                    avg = avg + Double(arrayOfNumbers[index])
+                }
+                avg = avg / Double(arrayOfNumbers.count)
+                print("The average is: \(avg)")
+                print("")
+            case "!", "fact":
+                var fact = 1
+                for var index = 1; index <= arrayOfNumbers[0]; index++ {
+                    fact = fact * index
+                }
+                print("\(arrayOfNumbers[0]) factorial is: \(fact)")
+                print("")
+            default:
+                print("You did not enter something valid.")
+                print("")
+        }
+    }
+    print("Would you like to calculate another problem: ", terminator: "")
+    test = input()
+    print("")
+} while(test == "yes")
